73d4c6ea80bb39529d6c3cc51a8eb99a9d26b76c
PieroCahuana <piero.cahuana@ucsm.edu.pe>
2025-03-25 12:12:42 -0500

    Se crean los metodos para probar GHappy en base a particiones

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,28 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class GHappyTest {
+
+    GHappy test1;
+
+     @BeforeEach
+     public void initialize(){
+         test1 = new GHappy();
+     }
+
+    @Test
+    public void todosPareja(){ //se usa los limites de inicio y fin
+        Assertions.assertTrue(test1.gHappy("ggxxgg")); //true
+    }
+
+    public void casiTodosPareja(){
+         Assertions.assertFalse(test1.gHappy("xgxggxgg")); //false
+    }
+
+    public void ningunaPareja(){
+         Assertions.assertFalse(test1.gHappy("xxxxxxxx")); //false
+    }
 }
