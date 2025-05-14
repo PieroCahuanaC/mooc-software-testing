23f5223df377f4d6c7cdba75aac93255a1cb4b26
PieroCahuana <piero.cahuana@ucsm.edu.pe>
2025-05-14 00:24:57 -0500

    Se agrega la correccion de error a la clase mirror

--- src/test/java/tudelft/mirror/MirrorTest.java
+++ src/test/java/tudelft/mirror/MirrorTest.java
@@ -1,4 +1,53 @@
 package tudelft.mirror;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MirrorTest {
+
+    @Test
+    public void testCoincidenciaCompleta() {
+        // palindromo completo
+        Mirror mirror = new Mirror();
+        assertEquals("aba", mirror.mirrorEnds("aba"));
+    }
+
+    @Test
+    public void testCoincidenciaParcial() {
+        // coincidencia donde solo la primera letra y la segunda coinciden
+        Mirror mirror = new Mirror();
+        assertEquals("ab", mirror.mirrorEnds("abXYZba"));
+    }
+
+    @Test
+    public void testSinCoincidencia() {
+        // no hay coincidencias
+        Mirror mirror = new Mirror();
+        assertEquals("", mirror.mirrorEnds("xyz"));
+    }
+
+    @Test
+    public void testCadenaVacia() {
+        Mirror mirror = new Mirror();
+        assertEquals("", mirror.mirrorEnds(""));
+    }
+
+    @Test
+    public void testUnSoloCaracter() {
+        Mirror mirror = new Mirror();
+        assertEquals("x", mirror.mirrorEnds("x"));
+    }
+
+    @Test
+    public void testCoincidenciaParcialCorrecta() {
+        Mirror mirror = new Mirror();
+        assertEquals("ab", mirror.mirrorEnds("abXYZba")); // 💥 Esto debería fallar si devuelve "ba"
+    }
+
+    @Test
+    public void testPalindromoParCompleto() {
+        Mirror mirror = new Mirror();
+        assertEquals("abba", mirror.mirrorEnds("abba"));
+    }
+
 }
